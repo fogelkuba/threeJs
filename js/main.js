ae1f64911e5303f5c0394f5c03e13fba3230eebf
fogelkuba <fogelkuba@gmail.com>
2018-02-25 10:21:48 +0100

    sphere

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
+// setup
 var scene = new THREE.Scene();
-
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 var renderer = new THREE.WebGLRenderer();
-
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
+
+
+// create shape
+var geometry = new THREE.SphereGeometry(1, 25, 25);
+
+// create material, color, image texture
+var material = new THREE.MeshBasicMaterial(
+    {
+        color: 0xffff00,
+        wireframe: true
+    })
+var sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+camera.position.z = 2;
 
 // game logic
 var update = function(){
-
+    sphere.rotation.x += 0.00;
+    sphere.rotation.y += .01;
 };
 
 // draw scene
@@ -24,4 +37,6 @@ var GameLoop = function(){
     update();
     render();
 };
+
+// init
 GameLoop();
